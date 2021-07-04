eda71beb58de8c70fabf08302893c287c338e6b7
Raphael Ribas <raphaelribas1@hotmail.com>
2021-07-04 13:37:17 -0300

    Correção Teste

--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import './Card.css';
 
 export default (props) => {
-
     return(
         <div className="card-prontuario">
             <h1>{props.title}</h1>
             {props.children}
         </div>
     )
-
 }
